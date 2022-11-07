4d7a9eeb3684e4bbaa87ad16285c655738db66aa
tineocac <carlostineocac@gmail.com>
2022-11-06 21:02:11 -0400

    seeders

---
+++ src/seeders/seeder.js
@@ -0,0 +1,44 @@
+const db = require("../utils/database");
+
+const Users = require("../models/users.models");
+const Address = require("../models/addresses.models");
+const Tasks = require("../models/tasks.models");
+const Categories = require("../models/categories.models");
+
+const users = [
+  { username: "Carlos", email: "carlos@gmail.com", password: "1234" },
+  { username: "Pedro", email: "pedro@gmail.com", password: "1234" },
+  { username: "Alejandro", email: "alejandro@gmail.com", password: "1234" },
+];
+
+const address = [
+  { street: "Calle 1", number: 5, user_id: 1 },
+  { street: "Calle 2", number: 3, user_id: 2 },
+  { street: "Calle 3", number: 10, user_id: 3 },
+];
+
+const tasks = [
+  { title: "New task 1", description: "Hacer la cena", userId: 1 },
+  { title: "New task 2", description: "Hacer la cena", userId: 2 },
+  { title: "New task 3", description: "Hacer la cena", userId: 3 },
+];
+
+const categories = [{ name: "Hogar" }, { name: "Tecno" }, { name: "Trabajo" }];
+
+db.sync({ force: false })
+  .then(() => {
+    console.log("Creating users");
+    users.forEach((user) => Users.create(user));
+  })
+  .then(() => {
+    console.log("Creating addreses");
+    address.forEach((addres) => Address.create(addres));
+  })
+  .then(() => {
+    console.log("Creating tasks");
+    tasks.forEach((task) => Tasks.create(task));
+  })
+  .then(() => {
+    console.log("Creating categories");
+    categories.forEach((category) => Categories.create(category));
+  });
